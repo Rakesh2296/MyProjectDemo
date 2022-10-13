5382dc7c9022e99cdd90b827125c7b7dfb8fc6fa
Rakesh2296 <rakeshdesale4@gmail.com>
2022-10-13 11:49:55 +0530

    ArrayList Demo Project is added to Local Repository.

---
+++ DemoArrayProject/src/com/demo/ArrayList.java
@@ -0,0 +1,33 @@
+package com.demo;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayList {
+
+	public static void main(String[] args) {
+
+//		List<String> list =  (List<String>) new ArrayList();
+		List<String> list = Arrays.asList("Rakesh","madahv");
+//		list.add("Rakesh");
+//		list.add("Manish");
+//		list.add("akash");
+//		list.add("madhav");
+		
+		System.out.println(list);	
+		
+	//	list.forEach((key)-> System.out.println(list));
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
