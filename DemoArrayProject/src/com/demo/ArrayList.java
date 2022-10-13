a15042e22863c987249b77f5b3ae8863816b667c
Rakesh2296 <rakeshdesale4@gmail.com>
2022-10-13 14:32:20 +0530

    Lambda expression use.

--- DemoArrayProject/src/com/demo/ArrayList.java
+++ DemoArrayProject/src/com/demo/ArrayList.java
@@ -8,14 +8,16 @@ public class ArrayList {
 	public static void main(String[] args) {
 
 //		List<String> list =  (List<String>) new ArrayList();
-		List<String> list = Arrays.asList("Rakesh","madahv");
-
+		List<String> list = Arrays.asList("Rakesh","madhav");
+		
+		
 		
 		for(String s: list) {
 			System.out.println(s);
 		}
 		
 		
+		list.forEach((key)-> System.out.print(key+" "));
 		
 		
 		
